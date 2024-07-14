8a2cf84aee1d5f5e2a77651004d1e395e924bd6b
alyssonmths <128157797+alyssonmths@users.noreply.github.com>
2024-07-14 00:31:17 -0300

    Implementação da funcionalidade de adicionar categorias

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ class Task {
 function gravar(tarefa) {
     // recupera o id gravado em LocalStorage e o atualiza depois da inserção da tarefa.
     let id = parseInt(localStorage.getItem('id'))
-    localStorage.setItem(`${id}`, JSON.stringify(tarefa))
+    localStorage.setItem(`task_${id}`, JSON.stringify(tarefa))
     id += 1
     localStorage.setItem('id', `${id}`)
 }
@@ -17,7 +17,10 @@ function gravar(tarefa) {
 function capturarDados() {
     let titulo = document.getElementById('titulo').value
     let descricao = document.getElementById('descricao').value
-    let imagem = document.getElementById('imagem').value
+
+    if (document.getElementById('imagem').value != '') {
+        var imagem = document.getElementById('imagem').value
+    }
     
     // criando um objeto a partir da classe Task, usando as variáveis declaradas acima
 
@@ -27,6 +30,7 @@ function capturarDados() {
     
     if (titulo != '' && descricao != '') { // sucesso
         gravar(tarefa)
+        // criando modal
         let modal = new bootstrap.Modal(document.getElementById('modal'))
         document.getElementById('titulo-modal').innerHTML = 'Sucesso'
         document.getElementById('conteudo-modal').innerHTML = 'A tarefa foi adicionada! Veja todas as suas tarefas em "Consultar"'
@@ -37,6 +41,7 @@ function capturarDados() {
         document.getElementById('imagem').value = ''
     }
     else { // erro
+        // criando modal
         let modal = new bootstrap.Modal(document.getElementById('modal'))
         document.getElementById('titulo-modal').innerHTML = 'Erro'
         document.getElementById('conteudo-modal').innerHTML = 'A tarefa não foi adicionada pois existem campos obrigatórios vazios. Corrija-os e tente novamente.'
@@ -45,3 +50,37 @@ function capturarDados() {
     }
 }
 
+function adicionarCategoria(id_categoria) {
+    let id = parseInt(id_categoria)
+    let categoria = document.getElementById('adicionar-categoria').value
+    if (categoria != '') {
+        localStorage.setItem(`categoria_${id_categoria}`, `${categoria}`)
+        id += 1
+        localStorage.setItem(`id_categoria`, `${id}`)
+
+        // criando modal
+        let modal = new bootstrap.Modal(document.getElementById('modal'))
+        document.getElementById('titulo-modal').innerHTML = 'Sucesso'
+        document.getElementById('conteudo-modal').innerHTML = 'Categoria criada com sucesso.'
+        document.getElementById('botao-modal').innerHTML = 'Fechar'
+        document.getElementById('botao-modal').setAttribute('onclick', 'window.location.reload()')
+        modal.show()
+    }
+    else {
+        let modal = new bootstrap.Modal(document.getElementById('modal'))
+        document.getElementById('titulo-modal').innerHTML = 'Erro'
+        document.getElementById('conteudo-modal').innerHTML = 'Campo vazio.'
+        document.getElementById('botao-modal').innerHTML = 'Fechar'
+        modal.show()
+    }
+}
+
+function lerCategorias(id_categoria) {
+    for (let index = 0; index < id_categoria; index++) {
+        let categoria = localStorage.getItem(`categoria_${index}`)
+        let select = document.getElementById('remover-categorias')
+        let option = document.createElement('option')
+        option.innerHTML = categoria
+        select.appendChild(option)
+    }
+}
